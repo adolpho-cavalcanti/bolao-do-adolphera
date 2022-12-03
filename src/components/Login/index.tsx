ee24963f5e5d3dc7fb7e7fd649b226626795fdb1
Adolpho Cavalcanti <adolpho.nascimento@compasso.com.br>
2022-12-03 12:04:17 -0300

    Retired login button

--- src/components/Login/index.tsx
+++ src/components/Login/index.tsx
@@ -1,4 +1,3 @@
-import { signIn } from "next-auth/react";
 import Image from "next/image";
 import iconCheck from '../../assets/icon-check.svg';
 
@@ -35,7 +34,6 @@ import usa from '../../assets/bandeiras/icon-united_states.svg';
 import uruguai from '../../assets/bandeiras/icon-uruguay.svg';
 import gales from '../../assets/bandeiras/icon-wales.svg';
 
-import logoGmail from '../../assets/gmail-logo.png';
 import logoSite from '../../assets/logo-adolpho.png';
 
 
@@ -51,7 +49,7 @@ const Login = ({ providers }) => {
             <span><strong className="text-xl">R$5 </strong> Dê o seu palpite e acompanhe os palpites dos seus adiversários</span>
           </div>
         </div>
-        {Object.values(providers).map((provider: any) => (
+        {/* {Object.values(providers).map((provider: any) => (
           <div key={provider.name}>
             <button 
               onClick={() => signIn(provider.id, { callbackUrl: "/" })}
@@ -61,7 +59,7 @@ const Login = ({ providers }) => {
               <Image src={logoGmail} alt="logo Gmail" width={20} />
             </button>
           </div>
-        ))}
+        ))} */}
         
       </nav>
 
